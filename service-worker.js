659221a1e1f584b84418524df6a5f2cdd5540071
Vijayakumari <vijaya@olivetech.net>
2018-03-27 19:16:57 +0530

    service workers sample basic

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+var cacheName = 'app-v1';
+var filesToCache = [
+  '/',
+  'images/cloudy_s_sunny.png',
+  'images/cloudy.png',
+  'images/rain.png',
+  'images/sleet.png'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      console.log('keyList',keyList);
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
